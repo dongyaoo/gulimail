49599109d6b3e73d9db18671ce7fd4675de7304a
东曜 <weixueguang.wxg@alibaba-inc.com>
2022-11-24 10:16:14 +0800

    feat(gulimail): 项目初始结构创建20221124

---
+++ gulimail-coupon/src/test/java/com/dongyaoo/gulimail/coupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongyaoo.gulimail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
